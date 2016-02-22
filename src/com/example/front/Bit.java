8cf14e7e8bb3f83d0c94ff4cb96ff889a88f40d1
agoodcoolman <agoodcoolman@163.com>
2016-02-23 00:17:16 +0800

    two
    two

--- src/com/example/front/Bit.java
+++ src/com/example/front/Bit.java
@@ -1,11 +1,14 @@
 package com.example.front;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.FrameLayout;
 
-public class Bit extends FrameLayout {
+public class Bit extends View {
 
 	public Bit(Context context) {
 		super(context);
@@ -17,7 +20,7 @@ public class Bit extends FrameLayout {
 		
 	}
 
-	public Bit(Context context, AttributeSet attrs, int defStyleAttr) {
+	@SuppressLint("NewApi") public Bit(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		setBackground(context.getResources().getDrawable(R.drawable.bit_01));
 	}
@@ -29,6 +32,10 @@ public class Bit extends FrameLayout {
 		setMeasuredDimension(1, 1);
 	}
 	
-	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// 不拦截处理任何事情
+		return false;
+	}
 
 }
